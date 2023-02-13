8845e442003167c122e53b246bf74f867ab98288
Arkadiusz Zygan <a.zygan@gmail.com>
2023-02-13 21:02:00 +0100

    Add create component

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -23,7 +23,7 @@ const FormField = ({
           <button
             type="button"
             onClick={handleSurpriseMe}
-            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
+            className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-black"
           >
             Surprise me
           </button>
@@ -37,7 +37,7 @@ const FormField = ({
         onChange={handleChange}
         required
         placeholder={placeholder}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring=[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full mb-2 p-3"
       />
     </div>
   );
