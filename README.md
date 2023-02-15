0f96059f27edbca29e761e3998439c06d5667596
Arkadiusz Zygan <a.zygan@gmail.com>
2023-02-15 20:09:56 +0100

    Deploy and add readme

---
+++ README.md
@@ -0,0 +1,42 @@
+# OPENAI Image generator
+
+### [LIVE DEMO](https://ai-image-generator-2023.netlify.app/)
+
+> The site allows you to generate images and share them after entering a phrase using the OPEN AI API
+
+### Used in project
+
+##### Frontend
+
+- react
+- vite
+- tailwindcss
+
+##### Backend
+
+- express
+- mongoose
+- openai
+- cloudinary
+- cors
+- nodemon
+
+### Installation
+
+To run client
+
+```sh
+cd ai-image-generator
+cd client
+npm i / yarn install
+npm start / yarn start
+```
+
+To run server
+
+```sh
+cd ai-image-generator
+cd server
+npm i / yarn install
+npm start / yarn start
+```
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -27,13 +27,16 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8888/api/v1/dalle", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ prompt: form.prompt }),
-        });
+        const response = await fetch(
+          "https://ai-image-generator-0czd.onrender.com/api/v1/dalle",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt: form.prompt }),
+          }
+        );
 
         const data = await response.json();
 
@@ -54,13 +57,16 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8888/api/v1/post", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ ...form }),
-        });
+        const response = await fetch(
+          "https://ai-image-generator-0czd.onrender.com/api/v1/post",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ ...form }),
+          }
+        );
         await response.json();
         navigate("/");
       } catch (error) {
